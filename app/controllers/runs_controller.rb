7e08f4a5cbbcf28b7f7a50f802c95dfe37a19315
Michal Kujawski <michal.kujawski@proofhq.com>
2016-10-17 13:55:46 +0200

    views - listing resources

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -1,4 +1,11 @@
 class RunsController < ApplicationController
+
   def index
+    @runs = Run.all
   end
+
+  def show
+    @run = Run.find(params[:id])
+  end
+
 end
--- app/controllers/suites_controller.rb
+++ app/controllers/suites_controller.rb
@@ -1,4 +1,11 @@
 class SuitesController < ApplicationController
+
   def index
+    @suites = Suite.all
   end
+
+  def show
+    @suite = Suite.find(params[:id])
+  end
+
 end
