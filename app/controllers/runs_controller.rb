d6f5ea59e7cc8acb0a7a3f341c835a6ab137a529
Michal Kujawski <michal.kujawski@proofhq.com>
2016-10-27 15:47:44 +0200

    chartjs added to lists

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -6,10 +6,28 @@ class RunsController < ApplicationController
 
   def show
     @run = Run.find(params[:id])
-    @tests = Test.where(run_id: params[:id])
+    @tests = @run.tests
 
-    # @tests_pass = @tests.group(:type).size
-    # true
+    results = @tests.group(:result_id).count(:result_id)
+    @stats = [0, 0, 0, 0, 0]
+
+    results.each do |result_id, result_count|
+      case result_id
+        when 1
+          @stats[0] += result_count
+        when 2,6
+          @stats[1] += result_count
+        when 3,7
+          @stats[2] += result_count
+        when 4,8
+          @stats[3] += result_count
+        when 5,9
+          @stats[4] += result_count
+        else
+          raise 'Unknown "result_id" in "test" table.'
+      end
+    end
+
   end
 
 end
