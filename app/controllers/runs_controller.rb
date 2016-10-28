8b8b61346a40a20f8181a38f41a4d44e005e4bb7
Michal Kujawski <michal.kujawski@proofhq.com>
2016-10-28 23:25:43 +0200

    improved result category collection

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -12,15 +12,13 @@ class RunsController < ApplicationController
     test_result_stats = @tests.select('results.category').joins(:result).group(:category).count(:category)
 
     # gather all result categories
-    categories = Result.all.group(:category).count(:category).keys
+    categories = Result.select(:category).group(:category).collect { |i| i.category }
 
     @stats = {}
 
     categories.each do |category|
       @stats[category.downcase.to_sym] = test_result_stats[category] || 0
     end
-
-    @stats
 
   end
 
