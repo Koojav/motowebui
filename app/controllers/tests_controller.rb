c52bca289c967d83e9bde2cb5e4bcdc5462359ea
Michal Kujawski <michal.kujawski@proofhq.com>
2017-03-07 14:42:47 +0100

    routes.rb - added redirections that simplify controllers controllers/test_controller - removed duplicate code that matched runs_controller's one (thanks to new redirections) view/runs/_details - unified naming of test results between buttons and graph legend view/... - changed default 10 items to 25 docker-compose.yml - added new value which will be used later on view/tests/list - added tooltip with full name version 1.0.4

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,10 +1,5 @@
 class TestsController < ApplicationController
 
-  def index
-    @run = Run.find(params[:run_id])
-    @tests = @run.tests.includes(:result)
-  end
-
   def show
     @test = Test.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
-  resources :suites,    only: [:index, :show] do
+  resources :suites,    only: [:index] do
     resources :runs,    only: [:index, :show]  do
-      resources :tests, only: [:index, :show]
+      resources :tests, only: [:show]
     end
   end
+
+  get '/suites/:suite_id', to: redirect('/suites/%{suite_id}/runs')
+  get '/suites/:suite_id/runs/:run_id/tests', to: redirect('/suites/%{suite_id}/runs/%{run_id}')
 
   namespace :api do
     resources :suites,      defaults: {format: :json} do
@@ -18,7 +21,6 @@ Rails.application.routes.draw do
 
     resources :testers, defaults: {format: :json}
     resources :results, defaults: {format: :json}
-
   end
 
   root 'suites#index'
