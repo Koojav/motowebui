07fa94865bcfe8feb9bec1cf00f134d30f33ac87
Michal Kujawski <koojav@gmail.com>
2018-01-31 09:48:42 +0100

    commented out unused columns

--- db/migrate/20161011111830_create_directories.rb
+++ db/migrate/20161011111830_create_directories.rb
@@ -7,13 +7,16 @@ class CreateDirectories < ActiveRecord::Migration[5.0]
       t.references  :parent, index: true, foreign_key: { to_table: :directories, on_delete: :cascade }
       t.references  :tester, foreign_key: true, default: 1
 
-      t.boolean     :stats_dirty, default: false
-      t.integer     :stats_tests_all,     default: 0
-      t.integer     :stats_tests_pass,    default: 0
-      t.integer     :stats_tests_error,   default: 0
-      t.integer     :stats_tests_fail,    default: 0
-      t.integer     :stats_tests_skip,    default: 0
-      t.integer     :stats_tests_running, default: 0
+      # Might be needed in the future in order to optimize reporting
+      # For now reports are on separate view, on request only, shouldn't pose much threat to performance
+
+      # t.boolean     :stats_dirty, default: false
+      # t.integer     :stats_tests_all,     default: 0
+      # t.integer     :stats_tests_pass,    default: 0
+      # t.integer     :stats_tests_error,   default: 0
+      # t.integer     :stats_tests_fail,    default: 0
+      # t.integer     :stats_tests_skip,    default: 0
+      # t.integer     :stats_tests_running, default: 0
     end
 
   end
