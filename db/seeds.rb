06155dab35759590a732b9328204e08452662a99
Michal Kujawski <koojav@gmail.com>
2018-02-02 14:48:21 +0100

    version 2.0.2 removed dev trash from seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,18 +16,6 @@ end
 
 if Directory.all.empty?
   Directory.create(path: '/', name: '/', id: 0)
-  dir3Aid = Directory.create_tree('/level1/level2/level3A')[:id]
-  dir3Bid = Directory.create_tree('/level1/level2/level3B')[:id]
 end
 
-if Test.all.empty?
-  Test.create([
-    {name: "Test 3A1", directory_id: dir3Aid},
-    {name: "Test 3A2", directory_id: dir3Aid},
-    {name: "Test 3A3", directory_id: dir3Aid},
-    {name: "Test 3B1", directory_id: dir3Bid},
-    {name: "Test 3B2", directory_id: dir3Bid},
-    {name: "Test 3B3", directory_id: dir3Bid},
-  ])
-end
 
